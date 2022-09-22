c46ed9b060eb2f9ccfc106812836d0ac0471051d
Hoon <dt@live.it>
2022-09-22 23:53:10 +0800

    feat: 新增默认布局

---
+++ layouts/DefaultLayout/default-layout.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Avatar, Layout, Space, Grid } from '@arco-design/web-react';
+import { IconArchive } from '@arco-design/web-react/icon';
+import Nav from '../../components/Nav';
+import { navConfig } from '../../config';
+
+const { Row, Col } = Grid;
+
+const DefaultLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Layout.Sider breakpoint='lg' collapsible>
+        <Nav
+          schema={navConfig.defaultLayoutSideNavSchema}
+          style={{ height: '100%' }}
+        />
+      </Layout.Sider>
+      <Layout>
+        <Layout.Header>
+          <Row
+            justify='space-between'
+            align='center'
+            style={{ padding: '0 20px' }}
+          >
+            <a>页面标题</a>
+            <Space>
+              <Nav
+                schema={navConfig.defaultLayoutTopNavSchema}
+                mode='horizontal'
+              />
+              <Avatar />
+              <span>用户名</span>
+            </Space>
+          </Row>
+        </Layout.Header>
+        <Layout style={{ padding: '24px', backgroundColor: '#f2f3f5' }}>
+          <Layout.Content style={{ backgroundColor: '#ffffff' }}>
+            {children}
+          </Layout.Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default DefaultLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../hooks/useAuth';
+import DefaultLayout from '../layouts/DefaultLayout';
+import '@arco-design/web-react/dist/css/arco.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
     </AuthProvider>
   );
 }
