878c93f431083829955a7f8ada3f525ad585164f
zexun.liu <zexun.liu@medalsoft.com>
2022-10-08 18:21:59 +0800

    feat: 添加布局判断

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,22 @@ import type { AppProps } from 'next/app';
 import { AuthProvider } from '../hooks/useAuth';
 import DefaultLayout from '../layouts/DefaultLayout';
 import '@arco-design/web-react/dist/css/arco.css';
+import { useRouter } from 'next/router';
+import LoginLayout from '../layouts/LoginLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  if (router.route === '/user/login' || router.route === '/user/register') {
+    return (
+      <AuthProvider>
+        <LoginLayout>
+          <Component {...pageProps} />
+        </LoginLayout>
+      </AuthProvider>
+    );
+  }
+
   return (
     <AuthProvider>
       <DefaultLayout>
