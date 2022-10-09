bc1cc52a58faebc88384ecd3412be093acc04aeb
hoon <dt@live.it>
2022-10-09 15:52:58 +0800

    fix: 修复中文乱码

--- hooks/useAuth/utils.ts
+++ hooks/useAuth/utils.ts
@@ -3,8 +3,20 @@ export const parseToken = (token: string | null) => {
     return false;
   }
   try {
+    token = token.replaceAll('-', '+').replaceAll('_', '/');
     const [, payload] = token.split('.');
-    return JSON.parse(window.atob(payload));
+    // debugger;
+    return JSON.parse(
+      decodeURIComponent(
+        window
+          .atob(payload)
+          .split('')
+          .map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join('')
+      )
+    );
   } catch (error) {
     console.warn('parse token failed');
     return false;
