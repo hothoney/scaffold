058701b3a5cffd8b8baa77e6645d778a4a6a7851
hoon <dt@live.it>
2022-10-09 17:53:51 +0800

    ci: 修改菜单配置

--- config/nav.tsx
+++ config/nav.tsx
@@ -1,3 +1,4 @@
+import { IconStorage } from '@arco-design/web-react/icon';
 import { type NavConfig } from './types';
 
 export default {
@@ -53,50 +54,20 @@ export default {
   defaultLayoutSideNavSchema: [
     {
       key: '0',
-      title: '侧边菜单 0',
+      title: '主数据维护',
+      icon: <IconStorage />,
       items: [
         {
           key: '0-1',
-          title: '侧边菜单 0-1',
-          items: [
-            {
-              key: '0-1-0',
-              title: '侧边菜单 0-1-0',
-            },
-          ],
+          title: '药物种类维护',
+          href: '/data/drug',
         },
         {
           key: '0-2',
-          title: '侧边菜单 0-2',
-          href: '/hello',
+          title: '宠物物种维护',
+          href: '/data/petSpecie',
         },
       ],
-    },
-    {
-      key: '1',
-      title: '侧边菜单 1',
-      items: [
-        {
-          key: '1-1',
-          title: '侧边菜单 1-1',
-        },
-        {
-          key: '1-2',
-          title: '侧边菜单 1-2',
-        },
-        {
-          key: '1-3',
-          title: '侧边菜单 1-3',
-        },
-      ],
-    },
-    {
-      key: '2',
-      title: '侧边菜单 2',
-    },
-    {
-      key: '3',
-      title: '侧边菜单 3',
     },
   ],
 } as NavConfig;
