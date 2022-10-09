4a5c37166880610db6fb5b5d802d7866a34c5897
Hoon <dt@live.it>
2022-10-10 00:15:28 +0800

    refactor: 精简页面

--- pages/data/drug/index.tsx
+++ pages/data/drug/index.tsx
@@ -1,21 +1,17 @@
-import React, { useState } from 'react';
 import {
-  Button,
   Table,
-  Modal,
   type TableColumnProps,
   Form,
   Input,
   Radio,
-  Message,
-  Typography,
+  type FormInstance,
+  Button,
 } from '@arco-design/web-react';
-import { useRequest } from 'ahooks';
-import axios from 'axios';
 import request from '../../../services/request';
 import { apisConfig } from '../../../config';
 import Head from 'next/head';
-import { IconPlus } from '@arco-design/web-react/icon';
+import CreateDataButton from '../../../components/CreateDataButton';
+import { IconDelete, IconEdit } from '@arco-design/web-react/icon';
 
 interface DrugData {
   id: number;
@@ -40,7 +36,7 @@ enum UsageEnum {
   Inhalation = 3,
 }
 
-const columns: TableColumnProps<DrugData>[] = [
+const columns: TableColumnProps<Partial<DrugData>>[] = [
   {
     title: '名称',
     dataIndex: 'name',
@@ -56,8 +52,54 @@ const columns: TableColumnProps<DrugData>[] = [
   {
     title: '操作',
     fixed: 'right',
+    width: 200,
+    render(col, item, index) {
+      return (
+        <>
+          <Button type='text' size='small'>
+            <IconEdit />
+            编辑
+          </Button>
+          <Button type='text' size='small' status='danger'>
+            <IconDelete />
+            删除
+          </Button>
+        </>
+      );
+    },
   },
 ];
+
+const FormComponent = (
+  form: FormInstance,
+  doRequest: (...args: any[]) => void
+) => (
+  <Form
+    layout='vertical'
+    form={form}
+    onSubmit={(value) => {
+      doRequest(value);
+    }}
+  >
+    <Form.Item field='name' label='名称' rules={[{ required: true }]}>
+      <Input />
+    </Form.Item>
+    <Form.Item field='usage' label='使用方式' rules={[{ required: true }]}>
+      <Radio.Group>
+        {Object.keys(UsageEnum)
+          .filter((usage) => isNaN(Number(usage)))
+          .map((usage) => (
+            <Radio value={(UsageEnum as any)[usage]} key={usage}>
+              {usage}
+            </Radio>
+          ))}
+      </Radio.Group>
+    </Form.Item>
+    <Form.Item field='description' label='描述' rules={[{ required: true }]}>
+      <Input.TextArea />
+    </Form.Item>
+  </Form>
+);
 
 const postDrugRequest = (data: DrugData) =>
   request<null>({
@@ -67,22 +109,6 @@ const postDrugRequest = (data: DrugData) =>
   });
 
 const index = () => {
-  const [isNewModalVisible, setIsNewModalVisible] = useState<boolean>(false);
-  const { run: postDrug } = useRequest(postDrugRequest, {
-    manual: true,
-    onSuccess(data, params) {
-      if (data.data.success) {
-        Message.success('新建成功');
-      } else {
-        Message.error('新建数据失败');
-      }
-    },
-    onError(e, params) {
-      Message.error('新建数据失败');
-    },
-  });
-  const [drugFormInstance] = Form.useForm();
-
   return (
     <>
       <Head>
@@ -91,55 +117,25 @@ const index = () => {
       <div style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 20 }}>
         药物数据管理
       </div>
-      <Modal
-        title='新建数据'
-        visible={isNewModalVisible}
-        onCancel={() => setIsNewModalVisible(false)}
-        onOk={() => drugFormInstance.submit()}
-      >
-        <Form
-          layout='vertical'
-          form={drugFormInstance}
-          onSubmit={(value) => {
-            postDrug(value);
-          }}
-        >
-          <Form.Item field='name' label='名称' rules={[{ required: true }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item
-            field='usage'
-            label='使用方式'
-            rules={[{ required: true }]}
-          >
-            <Radio.Group>
-              {Object.keys(UsageEnum)
-                .filter((usage) => isNaN(Number(usage)))
-                .map((usage) => (
-                  <Radio value={UsageEnum[usage]} key={usage}>
-                    {usage}
-                  </Radio>
-                ))}
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item
-            field='description'
-            label='描述'
-            rules={[{ required: true }]}
-          >
-            <Input.TextArea />
-          </Form.Item>
-        </Form>
-      </Modal>
-      <Button
-        type='primary'
-        onClick={() => setIsNewModalVisible(true)}
-        style={{ marginBottom: 10 }}
-      >
-        <IconPlus />
-        新建
-      </Button>
-      <Table columns={columns} />
+      <CreateDataButton
+        postRequest={postDrugRequest}
+        renderForm={FormComponent}
+      />
+      <Table
+        columns={columns}
+        data={[
+          {
+            name: '123',
+            usage: 0,
+            description: '123',
+          },
+          {
+            name: '123',
+            usage: 0,
+            description: '123',
+          },
+        ]}
+      />
     </>
   );
 };
