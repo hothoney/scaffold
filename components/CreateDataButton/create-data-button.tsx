aa7048f21bdfae2ba62e54497488a2ab27604ecf
hoon <dt@live.it>
2022-10-10 17:05:38 +0800

    refactor: 重构新建数据按钮

--- components/CreateDataButton/create-data-button.tsx
+++ components/CreateDataButton/create-data-button.tsx
@@ -1,28 +1,33 @@
-import { type FormInstance, Button, Modal } from '@arco-design/web-react';
+import {
+  type FormInstance,
+  Button,
+  Modal,
+  TableColumnProps,
+  Form,
+  Grid,
+  Input,
+} from '@arco-design/web-react';
 import request from '../../services/request';
 import { IconPlus } from '@arco-design/web-react/icon';
 import useCreateData from './useCreateData';
 
 interface CreateDataButtonProps<T> {
-  postRequest: (...args: any[]) => ReturnType<typeof request<T>>; // POST 方法
-  renderForm: (
-    form: FormInstance, // 表单实例
-    doRequest: (...args: any[]) => void // 执行请求方法
-  ) => JSX.Element;
+  columns: TableColumnProps[];
+  onSubmit?: (values: T) => Promise<boolean> | boolean;
+  onSuccess?: () => void;
+  onError?: (e?: unknown) => void;
 }
 
+const { Row, Col } = Grid;
+
 const CreateDataButton = <T,>({
-  postRequest,
-  renderForm,
+  columns,
+  onSubmit,
+  onError,
+  onSuccess,
 }: CreateDataButtonProps<T>) => {
-  const {
-    isNewModalVisible,
-    openModal,
-    closeModal,
-    formInstance,
-    postForm,
-    submitForm,
-  } = useCreateData<T>(postRequest);
+  const { isNewModalVisible, openModal, closeModal, formInstance } =
+    useCreateData();
 
   return (
     <>
@@ -30,11 +35,43 @@ const CreateDataButton = <T,>({
         title='新建数据'
         visible={isNewModalVisible}
         onCancel={closeModal}
-        onOk={submitForm}
+        onOk={async () => {
+          try {
+            await formInstance.validate();
+            const values = formInstance.getFieldsValue();
+            const result =
+              onSubmit &&
+              typeof onSubmit === 'function' &&
+              onSubmit(values as T);
+            if (result) {
+              onSuccess && typeof onSuccess === 'function' && onSuccess();
+              formInstance.resetFields();
+              closeModal();
+            } else {
+              onError && typeof onError === 'function' && onError();
+            }
+          } catch (error) {
+            onError && typeof onError === 'function' && onError(error);
+          }
+        }}
       >
-        {renderForm(formInstance, postForm)}
+        <Form form={formInstance}>
+          <Row>
+            {columns.map((column) => (
+              <Col span={24} key={column.key}>
+                <Form.Item
+                  field={column.dataIndex}
+                  label={column.title}
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+            ))}
+          </Row>
+        </Form>
       </Modal>
-      <Button type='primary' onClick={openModal} style={{ marginBottom: 10 }}>
+      <Button type='primary' onClick={openModal}>
         <IconPlus />
         新建
       </Button>
